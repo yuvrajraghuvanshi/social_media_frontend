07d67fe81e54d99492ca2a2f3acc233bd782a060
yuvrajraghuvanshi <yuvrajraghuvanshi070101@gmail.com>
2024-01-01 16:47:43 +0530

    login-system done;

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
@@ -8,16 +9,16 @@ import {
   Link,
   Routes
 } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 function App() {
+  const {user}=useContext(AuthContext)
   return(
     <Router>
     <Routes>
-    <Route exact path="/" element={<Home/>}>
-    </Route>
-    <Route path="/login" element={<Login/>}>
-    </Route>
-    <Route path="/register" element={<Register/>}>
-    </Route>
+      <Route exact path="/" element={user? <Home/>:<Register/>}> </Route> 
+   <Route exact path="/login" element={user? <Home/>:<Login/>}> </Route>
+<Route exact path="/register" element={user? <Home/>:<Register/>}> </Route>
+  
     <Route path="/profile/:username" element={<Profile/>}>
     </Route>
     </Routes>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,17 @@
+import { useContext, useRef } from "react";
+import {loginCall} from '../../apicalls'
 import "./login.css";
-
+import { AuthContext } from "../../context/AuthContext";
+import {CircularProgress} from "@material-ui/core"
 export default function Login() {
+  const username=useRef()
+  const password=useRef()
+  const {user,isFetching,error,dispatch}=useContext(AuthContext);
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    loginCall({username:username.current.value,password:password.current.value},dispatch)
+console.log(user)
+  }
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +22,15 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">Log In</button>
+          <form className="loginBox" onSubmit={handleSubmit}>
+            <input placeholder="Email" required className="loginInput" ref={username} />
+            <input placeholder="Password" type="password" required minLength="6" className="loginInput" ref={password} />
+            <button type="submit" disabled={isFetching} className="loginButton">{isFetching?<CircularProgress color="white"size="20px"/>:"Log In"}</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
